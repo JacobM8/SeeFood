09022bb2c7ee25c69cf34dd3855c3ff62eb7242a
jacobmorrison <iamjacobmorrison@gmail.com>
2023-07-25 14:15:16 -0400

    Create imagePicker, set as delegate, set imagePicker properties

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -12,15 +12,19 @@ import Vision
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera // allows users to take a photo with their front or back camera
+        imagePicker.allowsEditing = false
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         
+        present(imagePicker, animated: true, completion: nil)
     }
     
     
